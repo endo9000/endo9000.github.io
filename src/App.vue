a28773f317b3078b6c9315584ff4c4c0a2d6c447
endo9000 <deejayfromthailand+github@gmail.com>
2024-08-07 08:30:09 +0700

    installed shadcn-vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div class="my-2 flex justify-center">
+    <Button>Click me</Button>
+  </div>
+</template>
+
+<style scoped>
+</style>
